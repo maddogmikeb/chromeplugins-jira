beacdcce0276a805783ae2ed6b2333a05e5ca958
Mike Burns <maddogmikeb@users.noreply.github.com>
2020-01-31 13:01:45 +1000

    clonenode instead of creating

--- Jira Enricher/scripts/serviceDeskEnricher.js
+++ Jira Enricher/scripts/serviceDeskEnricher.js
@@ -38,6 +38,7 @@ function enrichServiceDeskQueues(options, document, expandedQueues) {
             if (!queues || !Array.isArray(queues)) continue;
             var total = 0;
 
+            var h = queues[0].cloneNode();
             var a = utils.createElementFromHTML(document, queues[0].innerHTML);
 
             queues.forEach(function(que) {
@@ -67,13 +68,14 @@ function enrichServiceDeskQueues(options, document, expandedQueues) {
             var arrow = utils.createElementFromHTML(document, `
                 <div style="position: absolute; right: 0;">
                     <span>
-                        <svg width="24" height="24" viewBox="0 0 24 24" focusable="false" role="presentation"><path d="M8.292 10.293a1.009 1.009 0 0 0 0 1.419l2.939 2.965c.218.215.5.322.779.322s.556-.107.769-.322l2.93-2.955a1.01 1.01 0 0 0 0-1.419.987.987 0 0 0-1.406 0l-2.298 2.317-2.307-2.327a.99.99 0 0 0-1.406 0z" fill="currentColor" fill-rule="evenodd"></path></svg>
+                        <svg width="24" height="24" viewBox="0 0 24 24" focusable="false" role="presentation">
+                            <path d="M8.292 10.293a1.009 1.009 0 0 0 0 1.419l2.939 2.965c.218.215.5.322.779.322s.556-.107.769-.322l2.93-2.955a1.01 1.01 0 0 0 0-1.419.987.987 0 0 0-1.406 0l-2.298 2.317-2.307-2.327a.99.99 0 0 0-1.406 0z" fill="currentColor" fill-rule="evenodd"></path>
+                        </svg>
                     </span>
                 </div>
             `);
             a.appendChild(arrow);
             a.addEventListener("click", onclick, false);
-            var h = utils.createElementFromHTML(document, `<div role="presentation" class="css-7xjigh"></div>`);
             h.appendChild(a);
             queues[0].parentNode.insertBefore(h, queues[0]);
         }
