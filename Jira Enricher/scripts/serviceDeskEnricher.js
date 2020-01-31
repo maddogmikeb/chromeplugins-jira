31ffcfadc27d02d2d89a521b0fae66e8d6a52d66
Mike Burns <maddogmikeb@users.noreply.github.com>
2020-01-31 12:51:59 +1000

    Add remove class properly

--- Jira Enricher/scripts/serviceDeskEnricher.js
+++ Jira Enricher/scripts/serviceDeskEnricher.js
@@ -9,14 +9,16 @@ function enrichServiceDeskQueues(options, document, expandedQueues) {
             var el = e.srcElement;
             while (!el.dataset.enriched) el = el.parentElement;
             if (el.dataset.enriched == "hide") {
-                document.querySelectorAll(`div[class='enrichedQueuesHide'][data-key='${el.dataset.key}']`).forEach(function(q) {
-                    q.className = "enrichedQueuesShow";
+                document.querySelectorAll(`div[class*='enrichedQueuesHide'][data-key='${el.dataset.key}']`).forEach(function(q) {
+                    q.classList.add("enrichedQueuesShow");
+                    q.classList.remove("enrichedQueuesHide");
                 })
                 el.dataset.enriched = "show";
                 expandedQueues.push(el.dataset.key);
             } else {
-                document.querySelectorAll(`div[class='enrichedQueuesShow'][data-key='${el.dataset.key}']`).forEach(function(q) {
-                    q.className = "enrichedQueuesHide";
+                document.querySelectorAll(`div[class*='enrichedQueuesShow'][data-key='${el.dataset.key}']`).forEach(function(q) {
+                    q.classList.add("enrichedQueuesHide");
+                    q.classList.remove("enrichedQueuesShow");
                 })
                 el.dataset.enriched = "hide";
                 expandedQueues.remByVal(el.dataset.key);
@@ -35,6 +37,9 @@ function enrichServiceDeskQueues(options, document, expandedQueues) {
             var queues = headerQueues[key];
             if (!queues || !Array.isArray(queues)) continue;
             var total = 0;
+
+            var a = utils.createElementFromHTML(document, queues[0].innerHTML);
+
             queues.forEach(function(que) {
                 que.querySelectorAll("div").forEach(function(t) {
                     if (t.innerText.includes(key + sep)) {
@@ -42,26 +47,31 @@ function enrichServiceDeskQueues(options, document, expandedQueues) {
                     }
                 });
                 que.dataset.key = key;
-                que.className = expandedQueues.includes(key) ? "enrichedQueuesShow" : "enrichedQueuesHide";
+                que.classList.add(expandedQueues.includes(key) ? "enrichedQueuesShow" : "enrichedQueuesHide");
                 try {
                     total += parseInt(que.querySelector("span").innerText);
                 } catch (e) {
                     console.error(`Jira Enricher: Unable to get count for ${key}`);
                 }
             });
-            var a = utils.createElementFromHTML(document, `<div draggable="false" data-testid="NavigationItem" data-key="${key}" class="css-16x8mro" data-enriched="${expandedQueues.includes(key) ? "show" : "hide"}" style="cursor: auto;"></div>`);
-            a.innerHTML = `
-                <div class="css-t44v0r">
-                    <div class="css-1olrtn">${key}</div>
-                </div>
-                <div class="css-9gcqfy">
-                    <span class="sc-elJkPf fteBTo">${total}</span>
-                </div>
-                <div class="css-1twqiqm" style="position: absolute; right: 0;">
-                    <span class="Icon__IconWrapper-dyhwwi-0 bcqBjl">
+
+            a.dataset.key = key;
+            a.dataset.enriched = expandedQueues.includes(key) ? "show" : "hide";
+            a.style = "cursor: auto;";
+            a.querySelectorAll("div").forEach(function(t) {
+                if (t.innerText.includes(key + sep)) {
+                    t.innerText = key;
+                }
+            });
+            a.querySelector("span").innerText = total;
+            var arrow = utils.createElementFromHTML(document, `
+                <div style="position: absolute; right: 0;">
+                    <span>
                         <svg width="24" height="24" viewBox="0 0 24 24" focusable="false" role="presentation"><path d="M8.292 10.293a1.009 1.009 0 0 0 0 1.419l2.939 2.965c.218.215.5.322.779.322s.556-.107.769-.322l2.93-2.955a1.01 1.01 0 0 0 0-1.419.987.987 0 0 0-1.406 0l-2.298 2.317-2.307-2.327a.99.99 0 0 0-1.406 0z" fill="currentColor" fill-rule="evenodd"></path></svg>
                     </span>
-                </div>`;
+                </div>
+            `);
+            a.appendChild(arrow);
             a.addEventListener("click", onclick, false);
             var h = utils.createElementFromHTML(document, `<div role="presentation" class="css-7xjigh"></div>`);
             h.appendChild(a);
